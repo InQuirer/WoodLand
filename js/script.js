17e21c0a74452fea6764cef3ea1440beb3934bb4
Habib Mohammad <vova313@gmail.com>
2015-05-14 06:09:35 +0300

    WoodLand v1.1
    Done: Dropdown, Pop-Up & DatePicker design
    Todo: unuser, refactoring, mobile.

--- js/script.js
+++ js/script.js
@@ -1,50 +1,56 @@
-/*$(".items").hover(function() {
+$("#dropDown, #items").hover(function () {
+	$("#items").css("z-index", "1");
 	$("#item1").fadeIn(100);
 	$("#item2").fadeIn(150);
 	$("#item3").fadeIn(200);
-}, function() {
-	$("#item1").delay(300).fadeOut();
-	$("#item2").delay(250).fadeOut();
-	$("#item3").delay(200).fadeOut();
-})*/
+}, function () {
+	if (this.id === "items") {
+		$("#item1").delay(300).fadeOut();
+		$("#item2").delay(250).fadeOut();
+		$("#item3").delay(200).fadeOut();
+	}
+});
 
-$(window).load(function() {
+$(window).load(function () {
 	$('.flexslider').flexslider({
 		animation: "slide"
 	});
 });
 
-$("#regBtn").click(function() {
+$("#regBtn").click(function () {
 	$("#hover").width($("#grid").width());
 	$("#hover").height($("body").height());
 	$("#hover").fadeIn();
 	$("#popup").fadeIn();
 });
 
-$("#hover, #cancel").click(function(){
+$("#hover, #cancel").click(function () {
 	$("#hover").fadeOut();
 	$("#popup").fadeOut();
 });
-//$(".flex-next").html("none");
 
-$(function(){
-	$(".items").hover(function(){
-		$(this).addClass("hover");
-		$('.link:first',this).css('visibility', 'visible');
-	}, function(){
-		$(this).removeClass("hover");
-		$('a:first',this).css('visibility', 'hidden');
+$(function () {
+	var countries = [
+		"Australia",
+		"Austria",
+		"Estonia"
+	];
+	$("#search").autocomplete({
+		source: countries
 	});
 });
 
-$("#menu").menu();
+$(function() {
+	$("#startDate").datepicker();
+	$("#endDate").datepicker();
+});
 
 function initialize() {
 	var mapOptions = {
 		zoom: 17,
 		center: new google.maps.LatLng(59.396695, 24.664854)
 	}
-	var map = new google.maps.Map(document.getElementById('gMap'),mapOptions);
+	var map = new google.maps.Map(document.getElementById('gMap'), mapOptions);
 	var image = 'images/beachflag.png';
 	var myLatLng = new google.maps.LatLng(59.396695, 24.664854);
 	var beachMarker = new google.maps.Marker({
